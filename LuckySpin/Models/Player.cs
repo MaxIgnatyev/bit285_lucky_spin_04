a26f9d4e1586db32c65cf1aaf73e4676eaa2ee49
maxignatyev <maxignatyev@gmail.com>
2019-01-22 15:17:14 -0800

    finish

---
+++ LuckySpin/Models/Player.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace LuckySpin.Models
+{
+    public class Player
+    {
+        //public Player()
+        //{
+        //}
+        public string Name { get; set; }
+        public int Lucky { get; set; }
+    }
+}
